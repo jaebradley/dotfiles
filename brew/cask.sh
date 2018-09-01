2517b6b9c37555f61e1ff5f97abda90b67911b64
Jae Bradley <jae.b.bradley@gmail.com>
2018-09-01 13:38:59 -0700

    alphabetize and organize brew cask and installs

--- brew/cask.sh
+++ brew/cask.sh
@@ -1,43 +1,71 @@
-brew cask install google-chrome
-brew cask install spectacle
-brew cask install dropbox
+##### Developer Tools #####
+
+# Miscelaneous
+brew cask install alfred
+brew cask install cakebrew
+brew cask install docker
+
+# Terminal
+brew cask install hyper
 brew cask install iterm2
+
+# Databases
+brew cask install mongodb
+brew cask install postico
+brew cask install psequel
+
+# HTTP
+brew cask install charles
+brew cask install insomnia
+brew cask install postman
+
+# GitHub
+brew cask install gitify
 brew cask install github
+
+# Java
+brew cask install intellij-idea
+brew cask install java
+
+# Text Editor (pick one or install them all)
 brew cask install visual-studio-code
 brew cask install atom
 brew cask install sublime-text
-brew cask install alfred
-brew cask install caffeine
-brew cask install joplin
-brew cask install docker
-brew cask install google-hangouts
-brew cask install flux
-brew cask install cheatsheet
-brew cask install firefox
-brew cask install hyper
-brew cask install the-unarchiver
-brew cask install vlc
-brew cask install vanilla
-brew cask install kap
-brew cask install cakebrew
-brew cask install spotify
-brew cask install postman
-brew cask install insomnia
-brew cask install charles
-brew cask install psequel
-brew cask install postico
+
+##### Essential Applications #####
+
 brew cask install bitbar
-brew cask install mongodb
+brew cask install cheatsheet
+brew cask install caffeine
+brew cask install dropbox
+brew cask install firefox
+brew cask install flux
+brew cask install flycut
+brew cask install google-chrome
+brew cask install istat-menus
+brew cask install joplin
+brew cask install kap
+brew cask install lastpass-cli --with-pinentry
+brew cask install numi
+brew cask install little-snitch
+brew cask install rescuetime
 brew cask install skitch
 brew cask install station
 brew cask install steam
+brew cask install spectacle
+brew cask install spotify
 brew cask install spotmenu
-brew cask install flycut
-brew cask install numi
-brew cask install lastpass-cli --with-pinentry
-brew cask install rescuetime
-brew cask install gitify
-brew cask install java
-brew cask install intellij-idea
-brew cask install istat-menus
-brew cask install little-snitch
+brew cask install the-unarchiver
+brew cask install vanilla
+brew cask install vlc
+
+
+
+
+
+
+
+
+
+
+
