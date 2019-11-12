2e6f75f92562aaa32309abcab70194fcb0994c03
Jae Bradley <jae.b.bradley@gmail.com>
2019-11-12 09:23:43 -0500

    Install vim plugins and add installation script to start script

---
+++ scripts/install_vim_plugins.sh
@@ -0,0 +1,19 @@
+echo "Create vim 8 plugins directory"
+mkdir -p ~/.vim/pack/plugins/start
+
+cd ~/.vim/pack/plugins/start
+
+git clone git@github.com:pangloss/vim-javascript.git
+git clone git@github.com:airblade/vim-gitgutter.git
+git clone git@github.com:vim-ruby/vim-ruby.git
+git clone git@github.com:tpope/vim-rails.git
+git clone git@github.com:tpope/vim-surround.git
+git clone git@github.com:dense-analysis/ale.git
+git clone git@github.com:scrooloose/nerdcommenter.git
+git clone git@github.com:mileszs/ack.vim.git
+git clone git@github.com/justinmk/vim.sneak.git
+git clone git@github.com/tpope/vim-repeat.git
+git clone git@github.com/valloric/MatchTagAlways.git
+git clone git@github.com:wellle/tmux-complete.vim.git
+git clone git@github.com:honza/vim-snippets.git
+
