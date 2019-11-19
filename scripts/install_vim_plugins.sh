dcaf0f065b6322993d371a47c3b7a6e00fdedd90
Jae Bradley <jae.b.bradley@gmail.com>
2019-11-19 11:18:50 -0500

    Add clone for NerdTree and update suffix list for vimrc (#60)

--- scripts/install_vim_plugins.sh
+++ scripts/install_vim_plugins.sh
@@ -35,6 +35,9 @@ git clone git@github.com:tpope/vim-surround.git
 # https://github.com/dense-analysis/ale
 git clone git@github.com:dense-analysis/ale.git
 
+# https://github.com/scrooloose/nerdtree
+git clone git@github.com:scrooloose/nerdtree.git
+
 # https://github.com/scrooloose/nerdcommenter
 git clone git@github.com:scrooloose/nerdcommenter.git
 
