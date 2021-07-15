705e9cb0375a8c49da127c9210e110fa3253442e
Jae Bradley <jae.b.bradley@gmail.com>
2021-07-15 09:22:02 -0400

    replace source-ing files with executing files

--- scripts/start.sh
+++ scripts/start.sh
@@ -45,46 +45,46 @@ installXcodeCommandLineTools
 
 # Install homebrew
 echo "Installing homebrew"
-source "$CURRENT_DIRECTORY/install_homebrew.sh"
+bash "$CURRENT_DIRECTORY/install_homebrew.sh"
 
 # Install homebrew packages
 echo "Installing homebrew packages"
-source "$CURRENT_DIRECTORY/../brew/install.sh"
+bash "$CURRENT_DIRECTORY/../brew/install.sh"
 
 # Check homebrew tap
 echo "Checking homebrew tap"
-source "$CURRENT_DIRECTORY/../brew/tap.sh"
+bash "$CURRENT_DIRECTORY/../brew/tap.sh"
 
 # Install homebrew cask
 echo "Checking homebrew casks"
-source "$CURRENT_DIRECTORY/../brew/cask.sh"
+bash "$CURRENT_DIRECTORY/../brew/cask.sh"
 
 installNvm
 
 # Install vim plugins
 echo "Installing vim plugins"
-source "$CURRENT_DIRECTORY/install_vim_plugins.sh"
+bash "$CURRENT_DIRECTORY/install_vim_plugins.sh"
 
 # Install oh-my-zsh
 echo "Installing oh-my-zsh"
-source "$CURRENT_DIRECTORY/install_zsh.sh"
+bash "$CURRENT_DIRECTORY/install_zsh.sh"
 
 # Install oh-my-zsh plugins
 echo "Installing oh-my-zsh plugins"
-source "$CURRENT_DIRECTORY/install_oh_my_zsh_plugins.sh"
+bash "$CURRENT_DIRECTORY/install_oh_my_zsh_plugins.sh"
 
 # Install mac-cli
 echo "Installing mac-cli"
-source "$CURRENT_DIRECTORY/install_mac_cli.sh"
+bash "$CURRENT_DIRECTORY/install_mac_cli.sh"
 
 # Setup Mac
 echo "Setting mac defaults"
-source "$CURRENT_DIRECTORY/../mac/setup.sh"
+bash "$CURRENT_DIRECTORY/../mac/setup.sh"
 
 # npm install globals
 echo "Installing global npm packages"
-source "$CURRENT_DIRECTORY/../npm/install.sh"
+bash "$CURRENT_DIRECTORY/../npm/install.sh"
 
 # Create ssh key
 echo "Creating ssh key"
-source "$CURRENT_DIRECTORY/generate_ssh_key.sh"
+bash "$CURRENT_DIRECTORY/generate_ssh_key.sh"
