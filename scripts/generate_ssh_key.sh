9420fb4b99308adf203cfd3f28fe2807d1ecb10f
Jae Bradley <jae.b.bradley@gmail.com>
2021-07-17 01:49:37 -0400

    specify output ssh key

--- scripts/generate_ssh_key.sh
+++ scripts/generate_ssh_key.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-ssh-keygen -t rsa -b 4096 -C "$1";
+ssh-keygen -t rsa -b 4096 -C "$1" -P "" -f ~/.ssh/id_rsa;
+if [[ 0 -ne $? ]]; then echo "error on line ${LINENO}" && exit 255; fi
 echo "Created ssh key"
 
 ssh-add -K ~/.ssh/id_rsa;
+if [[ 0 -ne $? ]]; then echo "error on line ${LINENO}" && exit 255; fi
 echo "Added ssh key to ssh-agent"
 
 pbcopy < ~/.ssh/id_rsa.pub;
+if [[ 0 -ne $? ]]; then echo "error on line ${LINENO}" && exit 255; fi
 echo "Copies the contents of the id_rsa.pub file to your clipboard";
 
 echo "Now go to GitHub and add ssh key"
