9790784f35b980d7b8e07263f9d1b07a6035dd97
Jae Bradley <jae.b.bradley@gmail.com>
2021-11-17 22:26:46 -0500

    Modify ssh algorithm (#92)

--- scripts/generate_ssh_key.sh
+++ scripts/generate_ssh_key.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-ssh-keygen -t rsa -b 4096 -C "$1" -P "" -f ~/.ssh/id_rsa;
+ssh-keygen -t ed25519 -C "$1" -P "" -f ~/.ssh/id_ed25519;
 if [[ 0 -ne $? ]]; then echo "error on line ${LINENO}" && exit 255; fi
 echo "Created ssh key"
 
-ssh-add -K ~/.ssh/id_rsa;
+ssh-add -K ~/.ssh/id_ed25519;
 if [[ 0 -ne $? ]]; then echo "error on line ${LINENO}" && exit 255; fi
 echo "Added ssh key to ssh-agent"
 
-pbcopy < ~/.ssh/id_rsa.pub;
+pbcopy < ~/.ssh/id_ed25519;
 if [[ 0 -ne $? ]]; then echo "error on line ${LINENO}" && exit 255; fi
 echo "Copies the contents of the id_rsa.pub file to your clipboard";
 
