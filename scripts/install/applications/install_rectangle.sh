89b554c857ed7b070861a150169f19393d4b4268
Jae Bradley <jae.b.bradley@gmail.com>
2021-11-17 22:27:08 -0500

    Install rectangle application script (#93)

---
+++ scripts/install/applications/install_rectangle.sh
@@ -0,0 +1,24 @@
+#!/bin/bash/
+
+function install_rectangle() {
+  curl -IL "https://github.com/rxhanson/Rectangle/releases/download/v0.49/Rectangle0.49.dmg"
+  if [[ $? -ne 0 ]]; then echo "Error on line $LINENO" && exit 255; fi
+
+  curl -L "https://github.com/rxhanson/Rectangle/releases/download/v0.49/Rectangle0.49.dmg" --output /tmp/rectangle.dmg
+  if [[ $? -ne 0 ]]; then echo "Error on line $LINENO" && exit 255; fi
+
+  hdiutil attach /tmp/rectangle.dmg
+  if [[ $? -ne 0 ]]; then echo "Error on line $LINENO" && exit 255; fi
+
+  local expectedVolumeDirectory="/Volume/Rectangle0.49"
+  if [[ ! -d  "${expectedVolumeDirectory}" ]]; then echo "${expectedVolumeDirectory} does not exist" && exit 255; fi
+
+
+  cp -R "${expectedVolumeDirectory}" /Applications/
+  if [[ $? -ne 0 ]]; then echo "Error on line $LINENO" && exit 255; fi
+
+  rm -rf /tmp/rectangle.dmg
+  if [[ $? -ne 0 ]]; then echo "Error on line $LINENO" && exit 255; fi
+}
+
+install_rectangle
